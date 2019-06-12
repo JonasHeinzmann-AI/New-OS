8ea02d0b1f1c3e2ea2d4ee3fca2ea0a9642c1e1c
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-12 10:26:50 +0200

    Update kernel.cpp
    Printf and the first action of the OS was added.

--- kernel.cpp
+++ kernel.cpp
@@ -1,1 +1,15 @@
+void printf(char* str)
+{
+    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
+    
+    for(int i=0; str[i]!='\0';++i){
+        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    }
+    
+}
 
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{ 
+    printf("Hello World");
+    while(1);
+}
