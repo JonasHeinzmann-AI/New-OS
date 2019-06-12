87503d657fac5f01492ee92394a9a11ac8411953
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-12 10:34:29 +0200

    Update loader.s

--- loader.s
+++ loader.s
@@ -1,1 +1,15 @@
+.section .text
+.extern kernelMain
+.global loader
 
+loader:
+    mov $kernel_stack, %esp    //setting the stack pointer
+    call kernelMain            //calling the kernelMain
+
+_stop:                        //extra precaution to keep running
+    cli
+    hlt
+    jmp _stop
+
+.section .bss 
+kernel_stack:
