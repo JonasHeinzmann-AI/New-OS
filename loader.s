ef81d76eeecdab16f03e0014542df7c4fc5d2649
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-12 10:36:33 +0200

    Update loader.s

--- loader.s
+++ loader.s
@@ -1,15 +1,29 @@
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot          // setting our magic number
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
 .section .text
 .extern kernelMain
+.extern callConstructors
 .global loader
 
 loader:
-    mov $kernel_stack, %esp    //setting the stack pointer
-    call kernelMain            //calling the kernelMain
+    mov $kernel_stack, %esp
+    call callConstructors
+    push %eax
+    push %ebx 
+    call kernelMain
 
-_stop:                        //extra precaution to keep running
+_stop:
     cli
     hlt
     jmp _stop
 
 .section .bss 
+.space 2*1024*1024;
 kernel_stack:
