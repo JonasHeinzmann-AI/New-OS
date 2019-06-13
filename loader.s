f622485397c1d50ca0bb416f3a397bff55a4f54a
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-13 20:27:48 +0200

    Update loader.s
    Updated version with some Bug fixes.

--- loader.s
+++ loader.s
@@ -2,28 +2,32 @@
 .set FLAGS, (1<<0 | 1<<1)
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-.section .multiboot          // setting our magic number
+.section .multiboot
     .long MAGIC
     .long FLAGS
     .long CHECKSUM
+
 
 .section .text
 .extern kernelMain
 .extern callConstructors
 .global loader
 
+
 loader:
     mov $kernel_stack, %esp
     call callConstructors
     push %eax
-    push %ebx 
+    push %ebx
     call kernelMain
+
 
 _stop:
     cli
     hlt
     jmp _stop
 
-.section .bss 
-.space 2*1024*1024;
+
+.section .bss
+.space 2*1024*1024; # 2 MiB
 kernel_stack:
