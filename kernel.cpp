dfc3181339f0b10cec0f3ceae4596c123e378034
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-13 20:55:16 +0200

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,27 +1,28 @@
+#include "types.h"
+
 void printf(char* str)
 {
-    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
-
-    for(int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
+    
+    for(int i=0; str[i]!='\0';++i){
+        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    }
+    
 }
-
-
 
 typedef void (*constructor)();
-extern "C" constructor start_ctors;
-extern "C" constructor end_ctors;
+extern "C" constructor* start_ctors;
+extern "C" constructor* end_ctors;
 extern "C" void callConstructors()
 {
-    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    for(constructor* i=start_ctors;i!=end_ctors;i++){
         (*i)();
+    }
 }
 
 
-
-extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
-{
-    printf("New-OS");
-
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{ 
+    printf("Hello World");
     while(1);
 }
