84ad053b5795ca3f52e87b498ef43d6bdc44e447
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-13 20:26:54 +0200

    Update kernel.cpp
    Updated version with some Bug fixes.

--- kernel.cpp
+++ kernel.cpp
@@ -1,15 +1,27 @@
 void printf(char* str)
 {
-    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
-    
-    for(int i=0; str[i]!='\0';++i){
-        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
-    }
-    
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
-extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
-{ 
-    printf("Hello World");
+
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+
+
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
+{
+    printf("New-OS");
+
     while(1);
 }
