37087a72f67d18dca867084edc67bfe6c6b5f858
Jonas Heinzmann <jon03hei@gmail.com>
2019-06-14 14:36:50 +0200

    Add files via upload
    Hardware-Communications, Ports and Interrupts added.

--- kernel.cpp
+++ kernel.cpp
@@ -1,28 +1,64 @@
 #include "types.h"
+#include "gdt.h"
+#include "interrupts.h"
 
 void printf(char* str)
 {
-    static unsigned short* VideoMemory=(unsigned short*)0xb8000;
-    
-    for(int i=0; str[i]!='\0';++i){
-        VideoMemory[i]=(VideoMemory[i] & 0xFF00)|str[i];
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    static uint8_t x=0,y=0;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+    {
+        switch(str[i])
+        {
+            case '\n':
+                x = 0;
+                y++;
+                break;
+            default:
+                VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
+                x++;
+                break;
+        }
+
+        if(x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if(y >= 25)
+        {
+            for(y = 0; y < 25; y++)
+                for(x = 0; x < 80; x++)
+                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
     }
-    
 }
+
+
 
 typedef void (*constructor)();
-extern "C" constructor* start_ctors;
-extern "C" constructor* end_ctors;
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
 extern "C" void callConstructors()
 {
-    for(constructor* i=start_ctors;i!=end_ctors;i++){
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
         (*i)();
-    }
 }
 
 
-extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
-{ 
-    printf("Hello World");
+
+extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
+{
+    printf("Hello World -- New OS\n");
+    printf("This is the version 0.0.0.3");
+    GlobalDescriptorTable gdt;
+    InterruptManager interrupts(0x20, &gdt);
+    interrupts.Activate();
+
     while(1);
 }
